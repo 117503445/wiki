0783e689e77ca81cb8e71ae7b4bf975009abda4b
117503445 <t117503445@gmail.com>
2020-12-07 11:53:02 +0800

    script win10_kms

--- docs/script.md
+++ docs/script.md
@@ -3,3 +3,5 @@
 [pip 设置清华镜像源](script/pip_tuna.sh)
 
 [Git 代理](script/git_proxy.sh)
+
+[Win10 激活](script/win10_kms.bat)
--- docs/script/win10_kms.bat
+++ docs/script/win10_kms.bat
@@ -1,14 +1,7 @@
-# Win10企业版激活
-
-保存为 bat 脚本
-
-基于 KMS 纯净无毒.
-
-```bat
 @ECHO OFF&PUSHD %~DP0
 
 setlocal EnableDelayedExpansion&color 3e & cd /d "%~dp0"
-title Windows 激活
+title Windows KMS
 
 %1 %2
 mshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :runas","","runas",1)(window.close)&goto :eof
@@ -19,4 +12,3 @@ slmgr.vbs /skms kms.03k.org
 slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
 slmgr.vbs /ato
 slmgr.vbs /xpr
-```
