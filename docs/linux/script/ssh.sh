a7af064ff23c92c0869647068cafe3279adc96d5
117503445 <t117503445@gmail.com>
2021-11-02 19:33:04 +0800

    Create ssh.sh

---
+++ docs/linux/script/ssh.sh
@@ -0,0 +1,10 @@
+###
+ # @Author: HaoTian Qi
+ # @Date: 2021-11-02 19:32:30
+ # @Description: 
+ # @LastEditTime: 2021-11-02 19:32:31
+ # @LastEditors: HaoTian Qi
+### 
+
+mkdir -p ~/.ssh && cd ~/.ssh
+echo ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDN7eJD8sBCLW80SRUaJh0LS4ClHL3CHPU4+fXOIpGS0Swlv8DuJFr29YnqRZTBhMJga4fQIKQAd0COJtRRncz8eniuMnOlY93cZoRMnOqCiGJG6iIuCCTT8C4f72bkgN6sYXSSP2nUPXXLw34Q/PRPg17RHaJuTnM+GmPfw1ThpvzdnfnJCYDxNqPpsmruqiz6V1W+u5mk7gM9Cjj7Lnq+TnZkVtzdM6zXb/pTEv7kaKyDAeD+O1PRAlRUP7zLeYxzuuIMAbcQzK2IEyjBACIlvakIx7fKAdqDD+8qSxCZLi4TArA9ELF6ji3zYvLeRf2z466TgyXwJrQo/vyLSLeV 117503445@SURFACE-PRO-5 > authorized_keys
